fac903edec10a44f82e58ecf13dd77d0b217076a
Ivan-IV <ivan-ivanicky@yandex.ru>
2022-09-01 14:39:29 +0300

    app-movies

--- movies-app/src/plugins/axios/index.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import interseptor from "./interseptor";
-const instance = axios.create({
-  baseURL: process.env.VUE_APP_URL_MOVE,
-});
-
-interseptor(instance);
-export default instance;
--- movies-app/src/plugins/bootstrap/index.js
+++
@@ -1,8 +0,0 @@
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
-import Vue from "vue";
-
-Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
